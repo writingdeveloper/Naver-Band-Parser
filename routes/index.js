86bc0273a8595fd40c67bed741fd1ca3cd7e434e
Si Hyeong Lee <super2451894@gmail.com>
2019-03-09 11:38:42 +0900

    Project Init
    - Project Init

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const request = require('request');
+
+const clientId='262096660';
+const clientSecret='zwJ0mEyKJNjWYDn8pxQrwmMjpxegzea_';
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  request(`https://auth.band.us/oauth2/authorize?response_type=code&client_id=${clientId}&redirect_uri=http://127.0.0.1:3000`, function(error, response, body){
+  if(error){
+    console.log(error);
+  }
+  console.log(`Status Code : ${response && response.statusCode}`)
+  // console.log(`Body : ${body}`)
+  // res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
+  res.end(body);
+  });
+  
+});
+
+module.exports = router;
